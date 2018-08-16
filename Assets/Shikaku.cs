4ef20388f8b750be555ff05d0b59ef679e9e30ce
John van Terheijden <john@fastguide.eu>
2018-08-16 17:05:09 +0200

    Work in progress.

--- Assets/Shikaku.cs
+++ Assets/Shikaku.cs
@@ -15,17 +15,22 @@ public class Shikaku : MonoBehaviour
     const int ShapePlus = 4;
     const int ShapeSquare = 5;
     const int ShapeH = 6;
-    const int ShapeN = 7;
-    const int ShapeZ = 8; // <- BigS, BigZ, SmallS, SmallZ
-    const int Shape2 = 9;
-    const int Shape3 = 10;
-    const int Shape4 = 11;
-    const int Shape5 = 12;
-    const int Shape6 = 13;
-    const int Shape7 = 14;
+    const int ShapeSmallS = 8;
+    const int ShapeSmallZ = 9;
+    const int ShapeLargeS = 10;
+    const int ShapeLargeZ = 11;
+    const int Shape2 = 12;
+    const int Shape3 = 13;
+    const int Shape4 = 14;
+    const int Shape5 = 15;
+    const int Shape6 = 16;
+    const int Shape7 = 17;
 
     const int Width = 6;
     const int Height = 6;
+
+    const int OutOfBounds = -1;
+    const int Empty = 0;
 
     enum Direction { Up, Right, Down, Left };
 
@@ -46,8 +51,10 @@ public class Shikaku : MonoBehaviour
         new ShapeType() { Shape = ShapePlus, Name = "Plus", MinSize = 5 },
         new ShapeType() { Shape = ShapeSquare, Name = "Square", MinSize = 4 },
         new ShapeType() { Shape = ShapeH, Name = "H", MinSize = 7 },
-        new ShapeType() { Shape = ShapeN, Name = "N", MinSize = 4 },
-        new ShapeType() { Shape = ShapeZ, Name = "Z", MinSize = 5 },
+        new ShapeType() { Shape = ShapeSmallS, Name = "Small S", MinSize = 4 },
+        new ShapeType() { Shape = ShapeSmallZ, Name = "Small Z", MinSize = 4 },
+        new ShapeType() { Shape = ShapeLargeS, Name = "Large S", MinSize = 5 },
+        new ShapeType() { Shape = ShapeLargeZ, Name = "Large Z", MinSize = 5 },
         new ShapeType() { Shape = Shape2, Name = "2", MinSize = 2 },
         new ShapeType() { Shape = Shape3, Name = "3", MinSize = 3 },
         new ShapeType() { Shape = Shape4, Name = "4", MinSize = 4 },
@@ -56,6 +63,8 @@ public class Shikaku : MonoBehaviour
         new ShapeType() { Shape = Shape7, Name = "7", MinSize = 7 }
     };
     private KMSelectable[] _buttons = new KMSelectable[36];
+    private TextMesh[] _hints = new TextMesh[36];
+    private string _hintChars = "ABABCDEFGHIJKLMNOOOOPPPPQRQRSTSTUVUVWXWXYZYZ222233334444555566667777";
     private List<Shape> _shapes = new List<Shape>();
 
     void Start()
@@ -64,9 +73,11 @@ public class Shikaku : MonoBehaviour
 
         for (int i = 0; i < 36; i++)
         {
-            _buttons[i] = Squares.transform.Find(i.ToString()).GetComponent<KMSelectable>();
             var j = i;
+            _buttons[i] = Squares.transform.Find(i.ToString()).GetComponent<KMSelectable>();
             _buttons[i].OnInteract += delegate () { PressButton(j); return false; };
+            _hints[i] = Squares.transform.Find(i.ToString()).transform.Find("Hint (" + i.ToString() + ")").GetComponent<TextMesh>();
+            _hints[i].text = "";
         }
 
         GeneratePuzzle();
@@ -79,15 +90,21 @@ public class Shikaku : MonoBehaviour
 
     private void GeneratePuzzle()
     {
-        int shapeNumber = 0;
-        while (shapeNumber < 5)
+        var puzzleTries = 0;
+        while (_shapes.Count < 5 && puzzleTries < 100)
         {
-            shapeNumber++;
+            puzzleTries++;
             //var shapeType = _shapeTypes[Rnd.Range(0, _shapeTypes.Length)];
-            var shapeType = _shapeTypes[ShapeLine];
-            var tries = 1;
-            while (!TryToAddShape(shapeType, shapeNumber)) tries++;
-            Debug.Log("Tries: " + tries.ToString());
+            var shapeType = _shapeTypes[ShapeL];
+            var shapeTries = 0;
+            var success = false;
+            while (shapeTries < 100)
+            {
+                shapeTries++;
+                success = TryToAddShape(shapeType, _shapes.Count + 1);
+                if (success) break;
+            }
+            DevLog((success ? "Succeeded" : "Failed") + " to add a " + shapeType.Name + " in " + shapeTries.ToString() + " tries");
         }
     }
 
@@ -96,7 +113,8 @@ public class Shikaku : MonoBehaviour
         // Random starting node
         int startNode;
         do startNode = Rnd.Range(0, _grid.Length);
-        while (_grid[startNode] != 0);
+        while (_grid[startNode] != Empty);
+        int cursorNode = startNode;
 
         var shape = new Shape()
         {
@@ -106,22 +124,56 @@ public class Shikaku : MonoBehaviour
             Nodes = new List<int>()
         };
 
-        // Draw basic shape
+        // Construct basic shape
         switch (shapeType.Shape)
         {
             case ShapeLine:
-                int node = startNode;
-                while (node != -1 && _grid[node] == 0)
+                while (cursorNode != OutOfBounds && _grid[cursorNode] == Empty)
                 {
-                    shape.Nodes.Add(node);
-                    node = Step(node, shape.Direction);
+                    shape.Nodes.Add(cursorNode);
+                    cursorNode = Step(cursorNode, shape.Direction);
                 }
-                if (shape.Nodes.Count < 2) return false;
+                if (shape.Nodes.Count < 2)
+                {
+                    DevLog("Failed to add a " + shapeType.Name + " at " + startNode + " directed " + shape.Direction + "; only room for 1.");
+                    return false;
+                }
+                break;
+            case ShapeL:
+                while (cursorNode != OutOfBounds && _grid[cursorNode] == Empty)
+                {
+                    shape.Nodes.Add(cursorNode);
+                    cursorNode = Step(cursorNode, shape.Direction);
+                }
+                cursorNode = Step(cursorNode, Turn180(shape.Direction));
+                if (shape.Nodes.Count < 2)
+                {
+                    DevLog("Failed to add a " + shapeType.Name + " at " + startNode + " directed " + shape.Direction + "; only room for 1.");
+                    return false;
+                }
+                // Direction part2Direction = Rnd.Range(0f, 1f) < .5 ? TurnLeft(shape.Direction) : TurnRight(shape.Direction);
+                Direction part2Direction = TurnLeft(shape.Direction);
+                var part2NodeCount = 1;
+                cursorNode = Step(cursorNode, part2Direction);
+                while (cursorNode != OutOfBounds && _grid[cursorNode] == Empty)
+                {
+                    shape.Nodes.Add(cursorNode);
+                    part2NodeCount++;
+                    cursorNode = Step(cursorNode, part2Direction);
+                }
+                if (part2NodeCount < 2)
+                {
+                    DevLog("Failed to add a " + shapeType.Name + " at " + startNode + " directed " + shape.Direction
+                        + ", part 2 going " + part2Direction + "; only room for 1");
+                    return false;
+                }
+
                 break;
         }
         shape.HintNode = shape.Nodes[Rnd.Range(0, shape.Nodes.Count)];
         _shapes.Add(shape);
         foreach (var node in shape.Nodes) _grid[node] = shapeNumber;
+        _hints[shape.HintNode].text = _hintChars[shape.ShapeType.Shape * 4 + (int)shape.Direction].ToString();
 
         return true;
     }
@@ -130,6 +182,21 @@ public class Shikaku : MonoBehaviour
     {
         var values = Enum.GetValues(typeof(Direction));
         return (Direction)values.GetValue(Rnd.Range(0, values.Length));
+    }
+
+    private Direction TurnRight(Direction direction)
+    {
+        return (Direction)(((int)direction + 1) % 4);
+    }
+
+    private Direction Turn180(Direction direction)
+    {
+        return (Direction)(((int)direction + 2) % 4);
+    }
+
+    private Direction TurnLeft(Direction direction)
+    {
+        return (Direction)(((int)direction + 3) % 4);
     }
 
     private int Step(int node, Direction direction)
@@ -163,6 +230,11 @@ public class Shikaku : MonoBehaviour
         for (var i = 0; i < _grid.Length; i++)
             log += _grid[i].ToString() + (i % Width == Width - 1 ? "\n" : "");
         Debug.Log(log);
+    }
+
+    private void DevLog(string message)
+    {
+        Debug.Log(message);
     }
 
     struct ShapeType
